3a70d8f0271ab723a6c868a3ab7a0e7f3b76c246
Krunal Modi <krunalmodi59@gmail.com>
2025-03-27 17:07:57 +0530

    Changed link

--- src/sxastarter/src/components/TitleDescriptionCarousel.tsx
+++ src/sxastarter/src/components/TitleDescriptionCarousel.tsx
@@ -12,7 +12,7 @@ import { GraphQLRequestClient } from '@sitecore-jss/sitecore-jss-nextjs/graphql'
 import { GET_USER_QUERY } from 'src/GraphQLQueries/MyQuery';
 
 const endpoint =
-  'http://xmc-sourceved15434-jsitecorexmc413-dev0494.sitecorecloud.io/sitecore/api/graph/edge';
+  'https://xmc-sourceved15434-jsitecorexmc413-dev0494.sitecorecloud.io/sitecore/api/graph/edge';
 const apiKey = '{B12D98C5-FB7B-48AE-8BF9-EBF7B0546DC9}';
 
 const client = new GraphQLRequestClient(endpoint, {
